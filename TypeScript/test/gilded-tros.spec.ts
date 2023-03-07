ff1d90d08b85aae82ccce008ad4642bec152d718
Jochem Janssens <janssens.jochem@gmail.com>
2023-03-07 19:21:16 +0100

    feat(Unit-tests): add test for sellIn date passed

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -1,7 +1,7 @@
 import {Item} from '../src/item';
 import {GildedTros} from '../src/gilded-tros';
 
-describe('End of the day: SellIn - 1 & Quality - 1', () => {
+describe('At the end of each day our system lowers both values (SellIn & Quality) for every item', () => {
     const items: Item[] = [new Item('item0', 1, 1), new Item('item1', 10, 1), new Item('item2', 5, 5)];
     const app: GildedTros = new GildedTros(items);
     app.updateQuality();
@@ -19,5 +19,26 @@ describe('End of the day: SellIn - 1 & Quality - 1', () => {
         expect(app.items[2].name).toEqual('item2');
         expect(app.items[2].sellIn).toEqual(4);
         expect(app.items[2].quality).toEqual(4);
+    });
+});
+
+describe('Once the sell by date has passed, Quality degrades twice as fast', () => {
+    const items: Item[] = [new Item('item0', -2, 10), new Item('item1', 0, 30), new Item('item2', -12, 40)];
+    const app: GildedTros = new GildedTros(items);
+    app.updateQuality();
+    test('check first item object', () => {
+        expect(app.items[0].name).toEqual('item0');
+        expect(app.items[0].sellIn).toEqual(-3);
+        expect(app.items[0].quality).toEqual(8);
+    });
+    test('check second item object', () => {
+        expect(app.items[1].name).toEqual('item1');
+        expect(app.items[1].sellIn).toEqual(-1);
+        expect(app.items[1].quality).toEqual(28);
+    });
+    test('check third item object', () => {
+        expect(app.items[2].name).toEqual('item2');
+        expect(app.items[2].sellIn).toEqual(-13);
+        expect(app.items[2].quality).toEqual(38);
     });
 });
